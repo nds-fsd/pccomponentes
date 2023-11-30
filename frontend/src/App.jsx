30c717b9294af7cc5ed19f3bb1895f33864439ee
Ruben Castillo <ruben26rcz@gmail.com>
2023-11-30 19:54:37 +0100

    refactor (react-router) delete reloads

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,26 @@
-import { useState } from "react";
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import Home from "./components/Home/Home"
 import ProductList from "./components/ProductList/ProductList"
 import Product from "./components/Product/Product"
 import NoMatch from "./components/NoMatch/NoMatch"
-import styles from "./App.module.css";
+import styles from "./app.module.css";
 
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  const [reload, setReload] = useState(false);
-
-  const reloadPage = () => setReload(!reload);
 
   return (
   <>
     <Header />
     <Routes>
-      <Route path="/" element={ <Home reload={reload}/> } />
-      <Route path="/product-list" element={ <ProductList reload={reload}/> }/>
-      <Route path="/product/:id" element={ <Product reload={reload}/> } />
+      <Route path="/" element={ <Home /> } />
+      <Route path="/product-list" element={ <ProductList /> }/>
+      <Route path="/product/:id" element={ <Product /> } />
       <Route path="*" element={ <NoMatch /> } />
     </Routes>
     <Footer />
   </>
-
-
   )
 }
 
