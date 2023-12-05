7c84f03bd23231d594c917be83b092ff724ec659
mauiriarte <miriarteh@gmail.com>
2023-12-05 13:10:25 +0100

    feat (PROD-CONT) added controller+router for product

--- backend/src/controllers/products.js
+++ backend/src/controllers/products.js
@@ -28,7 +28,41 @@ const postProduct = async (req, res) => {
     }
 }
 
+const getProductById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const ProductFound = await Product.findById(id)
+        return res.status(200).json({ ProductFound })
+    } catch (error) {
+        return res.status(404).json(error)
+    }
+}
+
+const patchProduct = async (req, res) => {
+    try {
+        const body = req.body
+        const { id } = req.params
+        const productUpdated = await Product.findByIdAndUpdate(id, body, { new: true })
+        return res.status(200).json({ productUpdated })
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+}
+
+const deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params
+        await Product.findByIdAndDelete(id)
+        res.status(201).json({ message: "Product deleted Succesfully" })
+    } catch (error) {
+        return res.status(404).json(error)
+    }
+}
+
 module.exports = {
     getProducts,
     postProduct,
+    getProductById,
+    patchProduct,
+    deleteProduct,
 }
