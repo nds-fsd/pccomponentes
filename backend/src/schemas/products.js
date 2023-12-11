1325775ca721ed0f145e95524c0bb714e87254bc
Artbat03 <artbat03@gmail.com>
2023-12-11 21:14:14 +0100

    feat (CATEG-SCHEMA) Created categories schemas

--- backend/src/schemas/products.js
+++ backend/src/schemas/products.js
@@ -1,13 +1,14 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const productSchema = new Schema({
-    name: { type: String, required: true },
-    brand: { type: String, required: true },
-    price: { type: Number, required: true },
-    description: { type: String, required: true },
-    stock: {type:Number, required: true},
+  name: { type: String, required: true },
+  brand: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  stock: { type: Number, required: true },
+  categories: [{ type: Schema.Types.ObjectId, ref: "category", required: true }]
 });
 
-const Product = model('product', productSchema);
+const Product = model("product", productSchema);
 
 module.exports = Product;
