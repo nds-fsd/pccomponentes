759a09fe9b6c1532a43a0851eb2335450e471424
Artbat03 <artbat03@gmail.com>
2023-12-14 13:37:48 +0100

    feat (DIREC-SCHEMA) Added direction schema to user

--- backend/src/schemas/users.js
+++ backend/src/schemas/users.js
@@ -1,13 +1,13 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const schema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   phoneNumber: Number,
-  direction: String
+  direction: { type: Schema.Types.ObjectId, ref: 'directions' }
 });
 
-const Users = model("Users", schema);
+const Users = model('users', schema);
 
 module.exports = Users;
