b8868ed98f69fd800fac01115cbe7f0805daa66e
Ruben Castillo <ruben26rcz@gmail.com>
2023-12-14 22:51:03 +0100

    fix(product.img-string): now image fills all the card

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -1,68 +1,15 @@
 import { Link } from 'react-router-dom';
 import styles from './productCard.module.css';
 
-function ProductCard({
-  product,
-}) {
+function ProductCard({ product }) {
   return (
-    <Link
-      to={`/${product._id}`}
-      className={
-        styles.product
-      }
-    >
-      <div
-        className={
-          styles.imagecontainer
-        }
-      >
-        <img
-          src={
-            product.image
-          }
-          alt="image of the product"
-        />
-      </div>
-      <span
-        className={
-          styles.chip
-        }
-      >
-        New
-      </span>
-      <div
-        className={
-          styles.text
-        }
-      >
-        <p
-          className={
-            styles.productBrand
-          }
-        >
-          {
-            product.brand
-          }
-        </p>
-        <p
-          className={
-            styles.productName
-          }
-        >
-          {
-            product.name
-          }
-        </p>
-        <p
-          className={
-            styles.productPrice
-          }
-        >
-          €
-          {
-            product.price
-          }
-        </p>
+    <Link to={`/${product._id}`} className={styles.product}>
+      <img src={product.image} alt='image of the product' />
+      <span className={styles.chip}>New</span>
+      <div className={styles.text}>
+        <p className={styles.productBrand}>{product.brand}</p>
+        <p className={styles.productName}>{product.name}</p>
+        <p className={styles.productPrice}>{product.price}€</p>
       </div>
     </Link>
   );
