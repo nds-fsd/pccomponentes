27e7d07a8839cb501b354fb50fca73fd95db2b6c
Ruben26rcz <ruben26rcz@gmail.com>
2024-02-09 19:44:33 +0100

    fix(nav-menu): categories naming changed

--- frontend/src/components/CategoryCard/CategoryCard.jsx
+++ frontend/src/components/CategoryCard/CategoryCard.jsx
@@ -4,8 +4,8 @@ import styles from './categoryCard.module.css';
 function CategoryCard({ category }) {
   return (
     <Link to={`/${category?._id}`} className={styles.product}>
-      <span className={styles.categoryName}>{category?.categoryName}</span>
-      <img src={category?.categoryImage} alt={category?.categoryName} />
+      <span>{category?.name}</span>
+      <img src={category?.image} alt={category?.name} />
     </Link>
   );
 }
