10be995c425f327af0ab472add6a937ae21d946f
Ruben26rcz <ruben26rcz@gmail.com>
2024-02-10 00:44:09 +0100

    feat(e2e-test): login test

--- e2e/cypress/e2e/auth/login.cy.js
+++ e2e/cypress/e2e/auth/login.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Login flow', () => {
+  // use these credentials to login
+  const email = 'ruben26rcz@gmail.com';
+  const password = '1234';
+
+  it('Can login', () => {
+    cy.visit('/');
+    cy.get('header a[href*="login"]').click();
+    cy.get('input[name="email"]').type(email);
+    cy.get('input[name="password"]').type(password);
+    cy.get('input[type="submit"]').click();
+  });
+});
