e7ee3a3b2ac8733d3d66c357838ca2eabf769a25
Artbat03 <artbat03@gmail.com>
2024-02-14 21:05:18 +0100

    fix (ORD-CONTR) Fixed orders schema

--- backend/src/schemas/orders.js
+++ backend/src/schemas/orders.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const ordersSchema = new Schema({
   user: { type: Schema.ObjectId, required: true, ref: 'User' },
-  products: [{ type: Schema.ObjectId, required: true, ref: 'Products' }],
+  products: [{ type: Schema.ObjectId, required: true, ref: 'Product' }],
   address: { type: Schema.ObjectId, required: true, ref: 'Address' },
 });
 
