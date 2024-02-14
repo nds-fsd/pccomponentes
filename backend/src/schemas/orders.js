a61a5417b6a7e4533000a45c2dd35cff4db33ebc
Artbat03 <artbat03@gmail.com>
2024-02-14 21:04:49 +0100

    feat (ORD-CONTR) Created endpoints for orders

---
+++ backend/src/schemas/orders.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const ordersSchema = new Schema({
+  user: { type: Schema.ObjectId, required: true, ref: 'User' },
+  products: [{ type: Schema.ObjectId, required: true, ref: 'Products' }],
+  address: { type: Schema.ObjectId, required: true, ref: 'Address' },
+});
+
+const Orders = model('Orders', ordersSchema);
+
+module.exports = Orders;
