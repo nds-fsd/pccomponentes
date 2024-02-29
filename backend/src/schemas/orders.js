d66b6199a7254b2b3d432e3a537c155b9d8fa83b
Artbat03 <artbat03@gmail.com>
2024-02-29 23:48:50 +0100

    feat (ORDS-BCKOFF) Added status tags in orders backoffice

--- backend/src/schemas/orders.js
+++ backend/src/schemas/orders.js
@@ -4,7 +4,12 @@ const ordersSchema = new Schema({
   user: { type: Schema.ObjectId, required: true, ref: 'User' },
   products: [{ type: Schema.ObjectId, required: true, ref: 'Product' }],
   address: { type: Schema.ObjectId, required: true, ref: 'Address' },
-  status: { type: String, enum: ['created', 'preparing', 'sent', 'delivered'], default: 'created', required: true },
+  status: {
+    type: String,
+    enum: ['created', 'preparing', 'sent', 'delivered', 'cancelled'],
+    default: 'created',
+    required: true,
+  },
 });
 
 const Orders = model('Orders', ordersSchema);
